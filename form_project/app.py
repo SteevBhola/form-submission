b2f8ed1477973f659b43e39c8dc6779857e8f217
SteevBhola <steevbhola68@gmail.com>
2025-04-20 14:30:13 +0530

    app.py

--- form_project/app.py
+++ form_project/app.py
@@ -66,5 +66,8 @@ def submit():
 
     return "Form submitted, PDF generated and email sent!"
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
